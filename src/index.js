f5d7115a0817d4cc6e70393b132e0359c7640041
jinyang.Zhang <jinyang.zhang@thoughtworks.com>
2021-06-02 22:21:45 +0800

    manage output

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import _ from "lodash";
-import './style.css';
-import Icon from './Simba.png';
-import Data from './data.xml';
+// import './style.css';
+// import Icon from './Simba.png';
+// import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement("div");
+  var btn = document.createElement('button');
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add('hello');
+  // element.classList.add('hello');
   // 将图像添加到我们已经存在的 div 中。
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  console.log(Data);
+  // const myIcon = new Image();
+  // myIcon.src = Icon;
+  // element.appendChild(myIcon);
+  // console.log(Data);
+  btn.innerHTML = '点击这里！';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,43 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    // entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
     mode: 'development',
     output: {
-        filename: 'bundle.js',
+        // filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    module: {
-        rules: [
-            {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-            },
-            {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-            },
-            {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            type: 'asset/resource',
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                'csv-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                'xml-loader'
-                ]
-            }
-        ],
-    },
+    // module: {
+    //     rules: [
+    //         {
+    //         test: /\.css$/i,
+    //         use: ['style-loader', 'css-loader'],
+    //         },
+    //         {
+    //         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+    //         type: 'asset/resource',
+    //         },
+    //         {
+    //         test: /\.(woff|woff2|eot|ttf|otf)$/i,
+    //         type: 'asset/resource',
+    //         },
+    //         {
+    //             test: /\.(csv|tsv)$/,
+    //             use: [
+    //             'csv-loader'
+    //             ]
+    //         },
+    //         {
+    //             test: /\.xml$/,
+    //             use: [
+    //             'xml-loader'
+    //             ]
+    //         }
+    //     ],
+    // },
 };
